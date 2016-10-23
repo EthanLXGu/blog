34a4d776bcf42f365c51f718bcfb54850408276d
Lingxin Gu <lingxingu0112@gmail.com>
2016-10-23 15:57:53 +0800

    add user model and show user in post

--- app/models/post.js
+++ app/models/post.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   information: DS.attr('string'),
-  comments: DS.hasMany('comment')
+  comments: DS.hasMany('comment'),
+  user: DS.belongsTo('user')
 });
