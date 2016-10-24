fed98fb76987c84d1174448304c7498872fcba24
Lingxin Gu <lingxingu0112@gmail.com>
2016-10-24 22:19:59 +0800

    modify some errors

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   information: DS.attr('string'),
   comments: DS.hasMany('comment'),
-  user: DS.belongsTo('user')
+  // user: DS.belongsTo('user')
 });
